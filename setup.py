8d20d74e3bae1957d256c2678c09264554170adb
AlexanderRicht <155488567+AlexanderRicht@users.noreply.github.com>
2024-02-10 12:15:20 +0100

    update commit

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="FredBrain",
-    version="0.1.0",
+    version="0.2.0",
     description="A Python API for retrieving Federal Reserve Economic Data at Scale and feeding it to OpenAI",
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     license=pathlib.Path("LICENSE").read_text(),
     url="https://github.com/AlexanderRicht/FredBrain",
+    project_urls={
+        "Documentation": "https://pypi.org/project/FredBrain/",
+        "Source": "https://github.com/AlexanderRicht/FredBrain",
+    },
     author="Alexander Richt",
     author_email="alexander.richt1@gmail.com",
     classifiers=[
